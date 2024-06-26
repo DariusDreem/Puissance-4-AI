c0efe78fd7ba4ae377de792d1aa6ae14b6aa7702
NathanReu <reungoat.nathan@gmail.com>
2024-06-26 17:42:28 +0200

    merged with BotLearnCsv

--- Player.py
+++ Player.py
@@ -1,8 +1,8 @@
 class Player:
-    __init__ = None
+    def __init__(self):
+        pass
 
-    def Play(self,column ,board, player_turn, columns_list) :
-         if column not in columns_list:
+    def Play(self, column, board, player_turn, columns_list):
+        if column not in columns_list:
             print("Invalid move. Try again.")
             return
-        
