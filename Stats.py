714a41d2451779d7a838e393aee7b299f3ad848b
Theo Stoffelbach <theo.stoffelbach@ynov.com>
2024-07-02 11:56:52 +0200

    Add error when file is empty

--- Stats.py
+++ Stats.py
@@ -21,7 +21,8 @@ class Puissance4CSV:
             os.makedirs("./Data/")
             open(self._fileDF, 'w').close()
             print("Fichier non trouvé. Un nouveau fichier sera créé.")
-
+        except pd.errors.EmptyDataError:
+            print("Pas de data dans csv dfOldGame!")
 
     def AjouterLigne(self, coord):
         nouvelle_paire = np.array([[coord[0], coord[1]]])
