32ac57db0ee508201c26659f108baec009b47f7b
Theo Stoffelbach <theo.stoffelbach@ynov.com>
2024-07-04 19:52:15 +0200

    fix MyCanva / Stats

--- MyCanva.py
+++ MyCanva.py
@@ -15,150 +15,193 @@ class MyCanvas(tk.Canvas):
     # ========================== MENU ==========================
 
     def __init__(self, master):
-        super().__init__(master)
-        self.master = master
-        self.configure(bg='white')
-        self.current_screen = None
-        self.rows = 6
-        self.cols = 7
-        self.cell_size = 100
-        self.board = self.master.game.board
-        self.width = 380
-        self.height = 350
-        self.config(width=self.width, height=self.height)
+        super().__init__(master)  # Appeler le constructeur de la classe parente
+        self.master = master  # Stocker la référence à l'objet maître
+        self.configure(bg='white')  # Configurer le fond de l'objet en blanc
+        self.current_screen = None  # Initialiser l'écran actuel à None
+        self.rows = 6  # Définir le nombre de lignes du tableau de jeu
+        self.cols = 7  # Définir le nombre de colonnes du tableau de jeu
+        self.cell_size = 100  # Définir la taille de chaque cellule
+        self.board = self.master.game.board  # Obtenir le tableau de jeu depuis l'objet maître
+        self.width = 380  # Définir la largeur de la fenêtre
+        self.height = 350  # Définir la hauteur de la fenêtre
+        self.config(width=self.width, height=self.height)  # Configurer les dimensions de la fenêtre
 
-        self.canvas = self
-        self.board = [[0] * self.cols for _ in range(self.rows)]
-        self.player = 1
-        self.show_menu()
-        print("MyCanvas init : ", self.master.game)
+        self.canvas = self  # Référence au canvas actuel
+        self.board = [[0] * self.cols for _ in range(self.rows)]  # Initialiser le tableau de jeu avec des zéros
+        self.player = 1  # Définir le joueur actuel à 1
+        self.show_menu()  # Afficher le menu principal
+        print("MyCanvas init : ", self.master.game)  # Afficher l'état du jeu pour le débogage
 
     def show_menu(self):
-        self.clear()
-        self.current_screen = "menu"
-        self.create_text(200, 40, text="Menu Screen", font=("Arial", 24),
-                         fill='black')
+        self.clear()  # Effacer le canvas
+        self.current_screen = "menu"  # Définir l'écran actuel sur "menu"
+        self.create_text(200, 40, text="Menu Screen", font=("Arial", 24), fill='black')  # Créer le texte du menu
+        # Ajouter des boutons pour différentes options de jeu
         MyButton(self, 200, 150, "Joueur contre Joueur", self.start_game_pvp)
-        MyButton(self, 200, 190, "Joueur contre Ordinateur",
-                 self.start_game_pve)
-        MyButton(self, 200, 230, "Ordinateur contre Ordinateur",
-                 self.start_game_ia)
-        MyButton(self, 200, 270, "Entraînement  IA",
-                 self.Start_training_ia)
-        MyButton(self, 200, 310, "Statistiques",
-                 self.Show_Stats)
+        MyButton(self, 200, 190, "Joueur contre Ordinateur", self.start_game_pve)
+        MyButton(self, 200, 230, "Ordinateur contre Ordinateur", self.start_game_ia)
+        MyButton(self, 200, 270, "Entraînement IA", self.Start_training_ia)
+        MyButton(self, 200, 310, "Statistiques", self.Show_Stats)
 
     def start_game_pvp(self):
-        self.clear()
-        self.master.game.gameType = GameType.GameType.PVP
-        print("Game Type : ", self.master.game.gameType)
-        self.setup_game()
+        self.clear()  # Effacer le canvas
+        self.master.game.gameType = GameType.GameType.PVP  # Définir le type de jeu sur "Joueur contre Joueur"
+        print("Game Type : ", self.master.game.gameType)  # Afficher le type de jeu pour le débogage
+        self.setup_game()  # Configurer le jeu
 
     def start_game_pve(self):
-        self.clear()
-        self.master.game.gameType = GameType.GameType.PVC
-        print("Game Type : ", self.master.game.gameType)
-        self.setup_game()
-        self.master.game.SetPlayerPVC()
+        self.clear()  # Effacer le canvas
+        self.master.game.gameType = GameType.GameType.PVC  # Définir le type de jeu sur "Joueur contre Ordinateur"
+        print("Game Type : ", self.master.game.gameType)  # Afficher le type de jeu pour le débogage
+        self.setup_game()  # Configurer le jeu
+        self.master.game.SetPlayerPVC()  # Configurer les joueurs pour "Joueur contre Ordinateur"
 
     def start_game_ia(self):
-        self.clear()
-        self.master.game.gameType = GameType.GameType.CVC
-        print("Game Type : ", self.master.game.gameType)
-        self.setup_game()
-        self.master.game.SetPlayerCVC()
+        self.clear()  # Effacer le canvas
+        self.master.game.gameType = GameType.GameType.CVC  # Définir le type de jeu sur "Ordinateur contre Ordinateur"
+        print("Game Type : ", self.master.game.gameType)  # Afficher le type de jeu pour le débogage
+        self.setup_game()  # Configurer le jeu
+        self.master.game.SetPlayerCVC()  # Configurer les joueurs pour "Ordinateur contre Ordinateur"
 
     def Start_training_ia(self):
-        self.clear()
-        self.trainningIA = True
-        self.master.game.gameType = GameType.GameType.CVC
-        print("Game Type : ", self.master.game.gameType)
-        self.setup_game()
-        self.master.game.SetPlayerCVC()
-        self.master.game.playTurn(-1)
+        self.clear()  # Effacer le canvas
+        self.trainningIA = True  # Activer le mode entraînement IA
+        self.master.game.gameType = GameType.GameType.CVC  # Définir le type de jeu sur "Ordinateur contre Ordinateur"
+        print("Game Type : ", self.master.game.gameType)  # Afficher le type de jeu pour le débogage
+        self.setup_game()  # Configurer le jeu
+        self.master.game.SetPlayerCVC()  # Configurer les joueurs pour "Ordinateur contre Ordinateur"
+        self.master.game.playTurn(-1)  # Commencer le tour de jeu pour l'IA
 
     def Show_Stats(self):
-        # self.clear()
+        # Analyser les premiers coups joués dans le jeu
         self.master.game.analyze_first_moves()
-        print("Show Stats Here ^^")
-
+        print("Show Stats Here ^^")  # Afficher un message de débogage
 
     def back_to_menu(self):
-        if self.current_screen == "game":
-            self.show_menu()
-            self.master.unbind('m')
+        if self.current_screen == "game":  # Vérifier si l'écran actuel est le jeu
+            self.show_menu()  # Afficher le menu
+            self.master.unbind('m')  # Détacher la touche 'm'
 
     def clear(self):
-        self.delete("all")
+        self.delete("all")  # Supprimer tous les éléments du canvas
+
 
     #======================= GAME =============================
 
     def setup_game(self):
-        self.root = self.master
-        self.root.title("Connect Four")
+        # Définir le nombre de lignes et de colonnes pour le jeu
         self.rows, self.cols = 6, 7
+
+        # Définir la taille de chaque cellule
         self.cell_size = 100
+
+        # Calculer la largeur et la hauteur totales de la fenêtre de jeu
         self.width = (self.cols * self.cell_size) + 10
         self.height = (self.rows + 1) * self.cell_size
+
+        # Configurer les dimensions de la fenêtre
         self.config(width=self.width, height=self.height)
+
+        # Dessiner le tableau de jeu initial
         self.draw_board()
+
+        # Mettre à jour le dessin du tableau de jeu avec l'état actuel
         self.update_draw_board()
 
     def handle_click(self, event):
+        # Vérifier si le jeu est terminé
+        if self.master.game.IsFinished:
+            return
+
+        # Vérifier si les clics sont désactivés ou si le type de jeu est CVC (Computer vs. Computer)
         if self.clickDeosntWork or self.master.game.gameType is GameType.GameType.CVC:
+            # Jouer un tour en fonction de la position du clic (colonne cliquée)
             self.master.game.playTurn(event.x // self.cell_size)
-        else :
+        else:
+            # Si les clics ne sont pas autorisés, afficher un message et désactiver les clics
             print("Click doesn't work")
             self.clickDeosntWork = True
 
 
     def draw_board(self):
-        for i in range(self.rows):
-            for j in range(self.cols):
+        for i in range(self.rows):  # Parcourir chaque ligne du tableau de jeu
+            for j in range(self.cols):  # Parcourir chaque colonne du tableau de jeu
+                # Calculer les coordonnées du coin supérieur gauche du rectangle
                 x0 = j * self.cell_size + 5
                 y0 = i * self.cell_size + 50
+
+                # Calculer les coordonnées du coin inférieur droit du rectangle
                 x1 = x0 + self.cell_size
                 y1 = y0 + self.cell_size
-                self.create_rectangle(x0, y0, x1, y1, outline="black")
-                if self.board[i][j] == 1:
-                    self.create_oval(x0 + 10, y0 + 10, x1 - 10, y1 - 10,
-                                     fill="red")
-                elif self.board[i][j] == 2:
-                    self.create_oval(x0 + 10, y0 + 10, x1 - 10, y1 - 10,
-                                     fill="yellow")
 
+                # Créer un rectangle pour représenter la cellule
+                self.create_rectangle(x0, y0, x1, y1, outline="black")
+
+                # Vérifier si la cellule contient un jeton du joueur 1
+                if self.board[i][j] == 1:
+                    # Créer un cercle rouge pour le jeton du joueur 1
+                    self.create_oval(x0 + 10, y0 + 10, x1 - 10, y1 - 10, fill="red")
+                # Vérifier si la cellule contient un jeton du joueur 2
+                elif self.board[i][j] == 2:
+                    # Créer un cercle jaune pour le jeton du joueur 2
+                    self.create_oval(x0 + 10, y0 + 10, x1 - 10, y1 - 10, fill="yellow")
+
+        # Associer un gestionnaire d'événements au canevas pour gérer les clics de souris pour les colones
         self.canvas.bind("<Button-1>", self.handle_click)
 
     def update_draw_board(self):
+        # Supprimer tous les éléments du canvas
         self.delete("all")
+
+        # Vérifier si le jeu est terminé
         if not self.master.game.IsFinished:
-            text = f"Joueur actuelle {self.master.game.player_turn}"
-        else :
+            # Le jeu n'est pas terminé, afficher le joueur actuel
+            text = f"Joueur actuel {self.master.game.player_turn}"
+        else:
+            # Le jeu est terminé, afficher le joueur gagnant
             text = f"Joueur gagnant {self.master.game.player_turn}"
 
+        # Créer le texte au centre du canvas
         self.create_text(self.winfo_reqwidth() // 2, self.fontSize, text=text,
                          font=("Helvetica", self.fontSize), fill="black")
+
+        # Mettre à jour le tableau avec l'état actuel du jeu
         self.board = self.master.game.board
+        # Dessiner le tableau de jeu
         self.draw_board()
+
         if not self.master.game.IsFinished:
+            # Si le jeu n'est pas terminé, réappeler cette méthode après 100 ms
             self.after(100, self.update_draw_board)
         else:
-            self.buttonRestart = tk.Button(self.root, text="Restart Game", command=self.restart_game)
+            # Si le jeu est terminé, créer un bouton pour redémarrer le jeu
+            self.buttonRestart = tk.Button(self.master, text="Restart Game", command=self.restart_game)
             self.buttonRestart.pack()
 
             print("self.trainningIA : ", self.trainningIA)
             if self.trainningIA:
+                # Si l'IA est en entraînement, redémarrer le jeu sans intervention de l'utilisateur
                 print("Training IA : New game")
                 self.buttonRestart.pack_forget()
                 self.master.game.ResetBoardGame()
                 self.Start_training_ia()
-                
+
+
 
     def restart_game(self):
+        # Cacher le bouton de redémarrage du jeu
         self.buttonRestart.pack_forget()
+
+        # Réinitialiser le tableau de jeu
         self.master.game.ResetBoardGame()
+
+        # Afficher le menu principal
         self.show_menu()
+
+        # Réinitialiser les dimensions de la fenêtre
         self.width = 230
         self.height = 500
-        self.clickDeosntWork = False
 
+        # Réinitialiser l'état du clic
+        self.clickDeosntWork = False
--- Stats.py
+++ Stats.py
@@ -1,30 +1,29 @@
 import os
-
 import pandas as pd
 import numpy as np
 
-
 class Puissance4CSV:
-    # create a list numpy array
-    arrayTurn = None
-    _scoreGame = None
-    _dfCSV = None
-    _fileDF = "Data/data.csv"
+    # Initialisation des variables de classe
+    arrayTurn = None  # Tableau pour stocker les coups joués
+    _scoreGame = None  # Tableau pour stocker les scores des parties
+    _dfCSV = None  # DataFrame pour stocker les données des parties
+    _fileDF = "Data/data.csv"  # Chemin du fichier CSV
 
     def __init__(self):
         # Initialisation des données
         print("Initialisation des données")
-        # #TEST IF FILE EXIST
         try:
-            self._dfCSV = pd.read_csv(self._fileDF, low_memory=False ,sep=';')
+            # Lecture du fichier CSV s'il existe
+            self._dfCSV = pd.read_csv(self._fileDF, low_memory=False, sep=';')
         except FileNotFoundError:
+            # Création du répertoire et du fichier CSV s'ils n'existent pas
             os.makedirs("./Data/")
             open(self._fileDF, 'w').close()
             print("Fichier non trouvé. Un nouveau fichier sera créé.")
-            
             print("Pas de data dans csv dfOldGame!")
 
     def AjouterLigne(self, coord):
+        # Ajouter une nouvelle paire de coordonnées au tableau des coups joués
         nouvelle_paire = np.array([[coord[0], coord[1]]])
         print("Nouvelle paire : ", nouvelle_paire)
         if self.arrayTurn is None:
@@ -32,17 +31,19 @@ class Puissance4CSV:
         else:
             self.arrayTurn = np.vstack([self.arrayTurn, nouvelle_paire])
 
-    def _CreateGameDF(self, newNumberOfThisGame=0,winner=1):
+    def _CreateGameDF(self, newNumberOfThisGame=0, winner=1):
+        # Créer un nouveau DataFrame pour une nouvelle partie
         data = {
             (f'Partie{newNumberOfThisGame}', 'Coordonnee'): [],
             (f'Partie{newNumberOfThisGame}.1', 'Score'): [],
         }
         self._dfCSV = pd.DataFrame(data)
 
+        # Ajouter le gagnant au DataFrame
         new_row = pd.DataFrame([['Winner', str(winner)]], columns=self._dfCSV.columns)
-
         self._dfCSV = pd.concat([new_row, self._dfCSV], ignore_index=True)
 
+        # Ajouter les coups joués et les scores au DataFrame
         new_row = {
             (f'Partie{newNumberOfThisGame}', 'Coordonnee'): self.arrayTurn.tolist(),
             (f'Partie{newNumberOfThisGame}.1', 'Score'): self._scoreGame.tolist()
@@ -51,18 +52,23 @@ class Puissance4CSV:
         self._dfCSV = pd.concat([self._dfCSV, new_row_df], ignore_index=True)
 
     def Sauvegarder(self, winner):
+        # Sauvegarder les données de la partie actuelle dans le fichier CSV
         df = pd.DataFrame()
         try:
-            df = pd.read_csv(self._fileDF, low_memory=False ,sep=';')
-            numberOfLastGame = int(df.columns[-2][6:]) # get penultimate column name and extract the number
+            # Lire le fichier CSV existant
+            df = pd.read_csv(self._fileDF, low_memory=False, sep=';')
+            # Extraire le numéro de la dernière partie enregistrée
+            numberOfLastGame = int(df.columns[-2][6:])
         except pd.errors.EmptyDataError:
             numberOfLastGame = 0
 
         print("self arrayTurn : ", self.arrayTurn)
 
+        # Initialiser les tableaux des coups pour chaque joueur
         p1Turn = np.array([self.arrayTurn[0]])
         p2Turn = np.array([self.arrayTurn[1]]) if len(self.arrayTurn) > 1 else np.array([])
 
+        # Répartir les coups joués entre les deux joueurs
         for i in range(len(self.arrayTurn) - 2):
             row = self.arrayTurn[i + 2]
             if (i + 2) % 2 == 0:
@@ -73,6 +79,7 @@ class Puissance4CSV:
                 else:
                     p2Turn = np.vstack([p2Turn, row])
 
+        # Générer les scores pour chaque joueur en fonction du gagnant
         if winner == 1:
             scoreP1 = np.array(self.generate_geometric_parts(p1Turn.shape[0], 1))
             scoreP2 = np.array(self.generate_geometric_parts(p2Turn.shape[0], -1))
@@ -80,43 +87,51 @@ class Puissance4CSV:
             scoreP1 = np.array(self.generate_geometric_parts(p1Turn.shape[0], -1))
             scoreP2 = np.array(self.generate_geometric_parts(p2Turn.shape[0], 1))
 
+        # Fusionner les scores des deux joueurs
         self._scoreGame = np.empty(scoreP1.size + scoreP2.size, dtype=scoreP1.dtype)
         self._scoreGame[0::2] = scoreP1
         self._scoreGame[1::2] = scoreP2
 
+        # Créer un nouveau DataFrame pour la nouvelle partie et l'ajouter au DataFrame existant
         self._CreateGameDF(numberOfLastGame + 1, winner)
 
         dfOldGame = pd.DataFrame()
-
         try:
+            # Lire le fichier CSV existant
             dfOldGame = pd.read_csv(self._fileDF, low_memory=False, sep=';', index_col=0, header=[0, 1])
         except pd.errors.EmptyDataError:
             print("Pas de data dans csv dfOldGame!")
 
+        # Afficher le DataFrame actuel et ancien pour le débogage
         print(self._dfCSV)
         print("========")
         print(dfOldGame)
+
+        # Combiner les DataFrames et les enregistrer dans le fichier CSV
         df_combined = pd.concat([dfOldGame, self._dfCSV], axis=1)
         print(df_combined)
         df_combined.to_csv(self._fileDF, sep=';')
+
+        # Réinitialiser le tableau des coups joués
         self.arrayTurn = None
 
     def AfficherDF(self):
+        # Afficher le DataFrame actuel
         print(self._dfCSV)
 
     def charger(self):
+        # Charger les données depuis le fichier CSV
         try:
-            self._dfCSV = pd.read_csv(self._fileDF,low_memory=False , sep=';')
+            self._dfCSV = pd.read_csv(self._fileDF, low_memory=False, sep=';')
             return self._dfCSV
         except FileNotFoundError:
             print("Fichier non trouvé. Un nouveau fichier sera créé.")
         except pd.errors.EmptyDataError:
             print("Pas de data !")
 
-
-    def generate_geometric_parts(self,num_parts, pointOfGame): # pointOfGame is 1 if win -1 if lose
+    def generate_geometric_parts(self, num_parts, pointOfGame):  # pointOfGame est 1 si victoire, -1 si défaite
+        # Générer une séquence géométrique pour les scores
         total_percentage = 100
-        # num_parts = 5
         ratio = 1.2
 
         # Calculer le premier terme a
@@ -126,13 +141,3 @@ class Puissance4CSV:
         parts = [(a * (ratio ** i)) for i in range(num_parts)]
 
         return parts
-
-# player_data.AjouterLigne([1, 4])
-# player_data.AjouterLigne([2, 4])
-# player_data.AjouterLigne([3, 5])
-# player_data.AjouterLigne([3, 4])
-# player_data.AjouterLigne([3, 2])
-# player_data.AjouterLigne([1, 2])
-# player_data.AjouterLigne([0, 2])
-
-# player_data.Sauvegarder(1)
